d37b29b14f03b793f886a83996bb4b0f03aa8742
kagasan <kaga.masaki.kj4@gmail.com>
2020-05-06 12:28:56 +0900

    領域抽出

--- image.hpp
+++ image.hpp
@@ -4,6 +4,73 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb-master/stb_image_write.h"
 #include <string>
+#include <sstream>
+
+class Point{
+    public:
+    int x, y;
+    Point(int x = 0, int y = 0):x(x),y(y){}
+    Point operator + (const Point p){
+        return Point(x + p.x, y + p.y);
+    }
+    std::string str(){
+        std::stringstream ss;
+        ss << "(" << x << ", " << y << ")";
+        return ss.str();
+    }
+};
+
+class int2d{
+    public:
+    int **data;
+    int width;
+    int height;
+    int2d(int width, int height, int val = 0):width(width),height(height){
+        data = new int*[height];
+        for(int i = 0; i < height; i++){
+            data[i] = new int[width];
+        }
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                data[y][x] = val;
+            }
+        }
+    }
+    ~int2d(){
+        for(int i = 0; i < height; i++){
+            delete[] data[i];
+        }
+        delete data;
+    }
+    int get(int x, int y){
+        return data[y][x];
+    }
+    int get(Point p){return get(p.x, p.y);}
+    void set(int x, int y, int val){
+        data[y][x] = val;
+    }
+    void set(Point p, int val){set(p.x, p.y, val);}
+    bool ok(int x, int y){
+        if(x < 0 || y < 0)return false;
+        if(x >= width || y >= height)return false;
+        return true;
+    }
+    bool ok(Point p){
+        return ok(p.x, p.y);
+    }
+    bool next(int &x, int &y){
+        x++;
+        if(x >= width){
+            x = 0;
+            y++;
+        }
+        if(y >= height)return false;
+        return true;
+    }
+    bool next(Point &p){
+        return next(p.x, p.y);
+    }
+};
 
 class Color{
     public:
@@ -30,6 +97,45 @@ class Color{
     }
 };
 
+Color HLStoRGB(double h, double l, double s){
+    Color rgb(0, 0, 0);
+    while(h >= 360)h -= 360;
+    double mx, mn;
+    if(l <= 49){
+        mx = 2.55 * (l + l * (s / 100));
+        mn = 2.55 * (l - l * (s / 100));
+    }else{
+        mx = 2.55 * (l + (100 - l) * (s / 100));
+        mn = 2.55 * (l - (100 - l) * (s / 100)); 
+    }
+    if (h < 60){
+        rgb.r = mx;
+        rgb.g = mn + (mx - mn) * (h / 60) ;
+        rgb.b = mn;
+    }else if (h >= 60 &&  h < 120){
+        rgb.r = mn + (mx - mn) * ((120 - h) / 60);
+        rgb.g = mx ;
+        rgb.b = mn;    
+    }else if (h >= 120 &&  h < 180){
+        rgb.r = mn;
+        rgb.g = mx ;
+        rgb.b = mn + (mx - mn) * ((h - 120) / 60);        
+    }else if (h >= 180 &&  h < 240){
+        rgb.r = mn;
+        rgb.g = mn + (mx - mn) * ((240 - h) / 60);
+        rgb.b = mx;     
+    }else if (h >= 240 &&  h < 300){
+        rgb.r = mn + (mx - mn) * ((h - 240) / 60);
+        rgb.g = mn;
+        rgb.b = mx;     
+    }else if (h >= 300 &&  h < 360){
+        rgb.r = mx;
+        rgb.g = mn;
+        rgb.b = mn + (mx - mn) * ((360 - h) / 60); 
+    }
+    return rgb;
+}
+
 class Image{
     private:
     unsigned int width;
@@ -42,19 +148,35 @@ class Image{
     Image(std::string filename);
     Image(unsigned int width = 640, unsigned height = 480);
     ~Image();
+    
     unsigned int getWidth();
     unsigned int getHeight();
     unsigned char getR(unsigned int x, unsigned int y);
     unsigned char getG(unsigned int x, unsigned int y);
     unsigned char getB(unsigned int x, unsigned int y);
+    unsigned char getR(Point p);
+    unsigned char getG(Point p);
+    unsigned char getB(Point p);
     Color getColor(unsigned int x, unsigned int y);
+    Color getColor(Point p);
+
     void setR(unsigned int x, unsigned int y, unsigned char r);
     void setG(unsigned int x, unsigned int y, unsigned char g);
     void setB(unsigned int x, unsigned int y, unsigned char b);
     void setRGB(unsigned int x, unsigned int y, unsigned char r, unsigned char g, unsigned char b);
     void setColor(unsigned int x, unsigned int y, Color color);
+    void setR(Point p, unsigned char r);
+    void setG(Point p, unsigned char g);
+    void setB(Point p, unsigned char b);
+    void setRGB(Point p, unsigned char r, unsigned char g, unsigned char b);
+    void setColor(Point p, Color color);
+    
     void read(std::string filename);
     void write(std::string filename);
+    bool next(int &x, int &y);
+    bool ok(int x, int y);
+    bool next(Point &p);
+    bool ok(Point p);
 };
 
 unsigned int Image::calculateIndex(unsigned int x, unsigned int y){
@@ -104,6 +226,10 @@ unsigned char Image::getB(unsigned int x, unsigned int y){
 Color Image::getColor(unsigned int x, unsigned int y){
     return Color(getR(x, y), getG(x, y), getB(x, y));
 }
+unsigned char Image::getR(Point p){return getR(p.x, p.y);}
+unsigned char Image::getG(Point p){return getG(p.x, p.y);}
+unsigned char Image::getB(Point p){return getB(p.x, p.y);}
+Color Image::getColor(Point p){return getColor(p.x, p.y);}
 
 void Image::setR(unsigned int x, unsigned int y, unsigned char r){
     pixels[calculateIndex(x, y)] = r;
@@ -124,6 +250,18 @@ void Image::setRGB(unsigned int x, unsigned int y, unsigned char r, unsigned cha
 void Image::setColor(unsigned int x, unsigned int y, Color color){
     setRGB(x, y, color.r, color.g, color.b);
 }
+
+void Image::setR(Point p, unsigned char r){setR(p.x, p.y, r);};
+void Image::setG(Point p, unsigned char g){setG(p.x, p.y, g);};
+void Image::setB(Point p, unsigned char b){setB(p.x, p.y, b);};
+void Image::setRGB(Point p, unsigned char r, unsigned char g, unsigned char b){
+    setRGB(p.x, p.y, r, g, b);
+}
+void Image::setColor(Point p, Color color){
+    setColor(p.x, p.y, color);
+}
+
+
 void Image::read(std::string filename){
     int w, h, b;
     pixels = stbi_load(filename.c_str(), &w, &h, &b, 0);
@@ -133,4 +271,28 @@ void Image::read(std::string filename){
 }
 void Image::write(std::string filename){
     stbi_write_png(filename.c_str(), width, height, bpp, pixels, 0);
+}
+
+bool Image::next(int &x, int &y){
+    x++;
+    if(x >= width){
+        x = 0;
+        y++;
+    }
+    if(y >= height)return false;
+    return true;
+}
+
+bool Image::ok(int x, int y){
+    if(x < 0 || y < 0)return false;
+    if(x >= width || y >= height)return false;
+    return true;
+}
+
+bool Image::next(Point &p){
+    return next(p.x, p.y);
+}
+
+bool Image::ok(Point p){
+    return ok(p.x, p.y);
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include "image.hpp"
 
+#include <random>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+std::random_device rd;
+std::mt19937_64 mt;
+Color randomColor(){
+    return HLStoRGB(mt() % 360, 60 + mt() % 30, 40 + mt() % 30);
+}
+
 void negaposi(std::string input, std::string output){
     Image src(input);
     int width = src.getWidth();
@@ -20,12 +31,48 @@ void negaposi(std::string input, std::string output){
     dst.write(output);
 }
 
+void colorful(std::string input, std::string output){
+    Image src(input);
+    Image dst(src.getWidth(), src.getHeight());
+    int2d mp(src.getWidth(), src.getHeight());
+    for(Point p; src.ok(p); src.next(p)){
+        if(src.getColor(p).ave() < 20)mp.set(p, -2);
+        else mp.set(p, -1);
+    }
+    Point dxy[4] = {Point(1, 0), Point(0, 1), Point(-1, 0), Point(0, -1)};
+    std::vector<Color>colorVec;
+    for(Point p; mp.ok(p); mp.next(p)){
+        if(mp.get(p) != -1)continue;
+        int id = colorVec.size();
+        colorVec.push_back(randomColor());
+        std::queue<Point>Q;
+        Q.push(p);
+        mp.set(p, id);
+        while(!Q.empty()){
+            for(auto d : dxy){
+                Point nxt = Q.front() + d;
+                if(!mp.ok(nxt))continue;
+                if(mp.get(nxt) != -1)continue;
+                Q.push(nxt);
+                mp.set(nxt, id);
+            }
+            Q.pop();
+        }
+    }
+    for(Point p; mp.ok(p); mp.next(p)){
+        if(mp.get(p) == -2)dst.setColor(p, src.getColor(p));
+        dst.setColor(p, colorVec[mp.get(p)]);
+    }
+    dst.write(output);
+}
+
 void controller(std::string input, std::string output){
-    negaposi(input, output);
+    // negaposi(input, output);
+    colorful(input, output);
 }
 
 int main(int argc, char* argv[]){
-
+    mt = std::mt19937_64(rd());
     if(argc == 3){
         controller(std::string(argv[1]), std::string(argv[2]));
     }
